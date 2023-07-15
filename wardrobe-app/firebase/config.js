4965dce62d164ab05693ff96487b153990596f84
Daniel Grace <graced@tcd.ie>
2023-07-15 17:28:53 +0100

    connected application to firebase and created register functionality

---
+++ wardrobe-app/firebase/config.js
@@ -0,0 +1,13 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export default firebaseConfig = {
+  apiKey: "AIzaSyC_6fLBGx6vPm39oHRhYGEmXqvsHM_CcBE",
+  authDomain: "wardrobe-app-54455.firebaseapp.com",
+  projectId: "wardrobe-app-54455",
+  storageBucket: "wardrobe-app-54455.appspot.com",
+  messagingSenderId: "19881156676",
+  appId: "1:19881156676:web:d8eb992809d3357b1fdc11",
+};
